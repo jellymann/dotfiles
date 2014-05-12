37592163d0fd1e848c4b4cf0c3a2ef1904055ce8
Daniel Smith <jellymann@gmail.com>
2014-05-13 01:30:42 +0200

    Removed git from install because that was a stupid mistake

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-DOTFILES="git gitconfig gitignore_global tmux tmux.conf tmuxinator vim vimrc zshrc"
+DOTFILES="gitconfig gitignore_global tmux tmux.conf tmuxinator vim vimrc zshrc"
 
 for f in $DOTFILES; do
   cp -r "$HOME/dotfiles/.$f" "$HOME/.$f"
